5512ae49e9e3c64c419f9e94673794463abd92e5
Kevin Gracie <kgracie12@gmail.com>
2016-04-19 21:57:15 +0100

    Added basic Scala files for working out how Objects were constructed and queried

---
+++ AdditionClass.scala
@@ -0,0 +1,7 @@
+class AdditionClass {
+	
+
+	def add(num1 : Int, num2 : Int) = {
+		num1+num2
+	}
+}
---
+++ UseAdditionClass.scala
@@ -0,0 +1,8 @@
+object UseAdditionClass {
+	
+	def main(args: Array[String]) {
+		var additionClass = new AdditionClass();
+		println("Result of addition = "+additionClass.add(12, 15))
+	}
+
+}
